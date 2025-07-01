acb7380376c08703918d85d9e72377d800da4164
chandan-16 <chandangautam420.cg@gmail.com>
2025-07-01 12:56:57 +0530

    minor changes made to grocerry component

--- src/components/Grocerry.js
+++ src/components/Grocerry.js
@@ -1,6 +1,6 @@
 const Grocerry = () => {
     return (
-    <div className="w-full px-4 sm:px-10 md:px-20 py-20 sm:py-32 md:py-40 lg:py-60 text-center">
+    <div className="w-full px-4 sm:px-10 md:px-20 py-20 sm:py-32 md:py-40 lg:py-60 text-center h-170">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800">
         Our Grocery Online Store
       </h1>
